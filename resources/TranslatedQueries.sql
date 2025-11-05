e4288af0b97a85a560fe7fb9571d619be1e1f114
roger-mahler <roger.mahler@hotmail.com>
2025-11-05 08:43:11 +0100

    fix: standardize SQL keywords to lowercase for consistency

--- resources/TranslatedQueries.sql
+++ resources/TranslatedQueries.sql
@@ -1,4 +1,4 @@
-SELECT
+select
     [persons].[full_name],
     [communities].[community_name],
     [entries].[year],
@@ -7,22 +7,20 @@ SELECT
     [seasons].[season_name],
     [entries].[land_rights_status],
     [entries].[original_placename],
-    [community_names].[Ortnamn],
+    [community_names].[ortnamn],
     [sources].[source_abbreviation],
     [entries].[reference_number],
-    [community_names].[LOPNR],
-    [community_names].[N],
-    [community_names].[E],
-    [entries].[ID]
-FROM ( ( ( ( ( sources
-INNER JOIN entries ON [sources].[source_id] = [entries].[source_id] )
-INNER JOIN seasons ON [seasons].[season_id] = [entries].[season_id] )
-INNER JOIN land_use ON [land_use].[land_use_id] = [entries].[resource_id] )
-INNER JOIN communities ON [communities].[community_id] = [entries].[community_id] )
-INNER JOIN persons ON [persons].[person_id] = [entries].[actor_id] )
-INNER JOIN community_names ON community_names.[ID] = entries.[placename_id];
+    [community_names].[lopnr],
+    [community_names].[n],
+    [community_names].[e],
+    [entries].[id]
+from ( ( ( ( ( sources
+inner join entries on [sources].[source_id] = [entries].[source_id] )
+inner join seasons on [seasons].[season_id] = [entries].[season_id] )
+inner join land_use on [land_use].[land_use_id] = [entries].[resource_id] )
+inner join communities on [communities].[community_id] = [entries].[community_id] )
+inner join persons on [persons].[person_id] = [entries].[actor_id] )
+inner join community_names on community_names.[id] = entries.[placename_id];
 
-
-
-SELECT [communities].[community_name]
-FROM [communities];
+select [communities].[community_name]
+from [communities];
