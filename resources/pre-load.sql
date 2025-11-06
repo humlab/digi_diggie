64aa9464a815e5278ac33c0272f464d00c3f994f
roger-mahler <roger.mahler@hotmail.com>
2025-11-06 14:09:45 +0100

    refactor: update pre-load.sql to modify geom column type and add geom_point column

--- resources/pre-load.sql
+++ resources/pre-load.sql
@@ -1,3 +1,15 @@
 
-\copy "placenames" ("fid", "geom", "ortnamn", "kvartsruta", "nkoordinat", "ekoordinat", "lanskod", "kommunkod", "detaljtyp", "sprak", "lopnummer", "sockenstadkod", "sockenstadnamn")
-from 'resources/ortnamn.csv' (format csv, delimiter E'\t', null '', header true, quote E'\\b');
+alter table placenames alter column geom type text using geom::text;
+
+\copy placenames(fid, geom, ortnamn, kvartsruta, nkoordinat, ekoordinat, lanskod, kommunkod, detaljtyp, sprak, lopnummer, sockenstadkod, sockenstadnamn) FROM 'resources/ortnamn.csv' WITH (FORMAT csv, HEADER true);
+
+alter table placenames
+  add column geom_point geometry(point, 3006);  -- or 4326, depending on crs
+
+update placenames
+  set geom_point = st_setsrid(st_makepoint(ekoordinat, nkoordinat), 3006);
+
+
+/* example use:
+SELECT ST_AsGeoJSON(ST_Transform(geom, 4326)) FROM placenames;
+*/
