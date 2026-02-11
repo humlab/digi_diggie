535c0e85ec635d08fa8036f2517b7cac3a56f3f8
aibakeneko <aibakeneko@gmail.com>
2026-02-11 15:42:12 +0100

    fix: clean up pre-load.sql by removing unused table definitions and comments. Fixes #2

--- resources/pre-load.sql
+++ resources/pre-load.sql
@@ -1,42 +1,6 @@
 -- run drop_all first to ensure clean state
 
----------
 drop table if exists public.placenames cascade;
--- create table if not exists "placenames"
---  (
---     "fid" integer primary key,
---     "geom" text,
---     "ortnamn" text,
---     "kvartsruta" text,
---     "nkoordinat" double precision,
---     "ekoordinat" double precision,
---     "lanskod" text,
---     "kommunkod" text,
---     "detaljtyp" text,
---     "sprak" text,
---     "lopnummer" text,
---     "sockenstadkod" text,
---     "sockenstadnamn" text
--- );
--- ---------
-
--- create table if not exists "placenames"
---  (
---     "ID" serial primary key,
---     "Ortnamn" text,
---     "N" double precision,
---     "E" double precision,
---     "LOPNR" text,
---     "Namntyp_nr" text,
---     "Språk_nr" text,
---     "Sockenstad_nr" text,
---     "Län_nr" text,
---     "Kommun_nr" text,
---     "Kombo" text,
---     "Sockenstad" text,
---     "nr" text    
--- );
-
 
 create table if not exists "placenames"
  (
@@ -55,19 +19,4 @@ create table if not exists "placenames"
     "nr" text    
 );
 
---alter table placenames alter column geom type text using geom::text;
-
---\copy placenames(fid, geom, ortnamn, kvartsruta, nkoordinat, ekoordinat, lanskod, kommunkod, detaljtyp, sprak, lopnummer, sockenstadkod, sockenstadnamn) FROM 'resources/placenames.csv' WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
---\copy placenames("Ortnamn", "N", "E", "LOPNR", "Namntyp_nr", "Språk_nr", "Sockenstad_nr", "Län_nr", "Kommun_nr", "Kombo", "Sockenstad", "nr") FROM 'resources/placenames.csv' WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
-\copy placenames(ortnamn, n, e, lopnr, namntyp_nr, språk_nr, sockenstad_nr, lan_nr, kommun_nr, kombo, sockenstad, nr) FROM 'resources/placenames.csv' WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
-
---alter table placenames
-  add column geom_point geometry(point, 3006);  -- or 4326, depending on crs
-
---update placenames
-  set geom_point = st_setsrid(st_makepoint(ekoordinat, nkoordinat), 3006);
-
-
-/* example use:
-SELECT ST_AsGeoJSON(ST_Transform(geom, 4326)) FROM placenames;
-*/
+\copy placenames(id, ortnamn, n, e, lopnr, namntyp_nr, språk_nr, sockenstad_nr, lan_nr, kommun_nr, kombo, sockenstad, nr) FROM 'resources/placenames.csv' WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
