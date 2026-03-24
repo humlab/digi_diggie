4d36178c93412550cb78b6f597a66ea89add6377
roger-mahler <roger.mahler@hotmail.com>
2026-03-24 14:02:50 +0100

    feat: migrate person outcomes to new schema

--- src/schema/tog2tng.sql
+++ src/schema/tog2tng.sql
@@ -266,13 +266,6 @@ begin
           on r.winner_id = tog.winner_id;
 
     /***********************************************************************************************************
-    ** STEP     Add person outcomes.
-    ************************************************************************************************************/
-
-	select *
-	from digidiggie_tng.person_outcome
-
-    /***********************************************************************************************************
     ** STEP     Add person relationships. Currently, the data lacks relationship information.
     ************************************************************************************************************/
 
@@ -614,6 +607,34 @@ begin
     -- create rulings for cases that have winner_id, judgement_id, or legal_source_id
     -- note: ruling_type is a new concept, we'll need to populate it separately
 
+	with tog_to_tng as (
+		select cc.court_case_id, tog.entry_id as tog_id, tng.court_case_entry_id as tng_id
+		from digidiggie_tog.entries tog
+		join digidiggie_tng.court_case_entry tng
+		  on tog.season_id = tng.season_id
+		 and tog.land_use_id = tng.land_use_id
+		 and coalesce(tog.placename_id, -1) = coalesce(tng.placename_id, -1)
+		join digidiggie_tng.court_case cc
+		  on tng.court_case_id = cc.court_case_id
+		 and lower(trim(cc.reference_number)) = lower(trim(tog.reference_number))
+		 and tog."year"::int = tng.entry_year
+	), court_case_ruling as (
+		select court_case_id, ruling_id
+		from digidiggie_tng.ruling
+		join digidiggie_tng.court_case using (court_case_id)
+	)
+	insert into digidiggie_tng.person_outcome(ruling_id, person_id, outcome_type_id, description)
+		select ccr.ruling_id,
+			coalesce(tog.actor_id, 0) as person_id,
+			case when tog.judgement_id in (1, 2, 3, 4, 5, 6, 7) then tog.judgement_id else null end as outcome_type_id,
+			'' as description
+		from digidiggie_tog.entries tog
+		join tog_to_tng m
+		on m.tog_id = tog.entry_id
+		join court_case_ruling ccr 
+		on m.court_case_id = ccr.court_case_id
+		join digidiggie_tog.judgements j using (judgement_id)
+		where tog.judgement_id in (1, 2, 3, 4, 5, 6, 7);
 
     raise notice 'step: migrating persons'' outcomes...';
 
